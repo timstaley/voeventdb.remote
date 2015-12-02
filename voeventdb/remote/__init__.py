17085b67aca6dc10f09721b28a75b05b65c5a063
Tim Staley <tim.staley@physics.ox.ac.uk>
2015-12-02 15:07:23 +0000

    Make explicit that functions are apiv0, fill out endpoint calls.

--- voeventdb/remote/__init__.py
+++ voeventdb/remote/__init__.py
@@ -1,12 +1,4 @@
 from __future__ import absolute_import
-from voeventdb.remote.convenience import (
-    count,
-    ivorn,
-    ivorn_cited_count,
-    ivorn_ref_count,
-    synopsis,
-    xml,
-)
 default_host = 'http://voeventdb.4pisky.org'
 
 default_pagesize = 1000
