fa9c61e37e19c8f8e71385203135bc61c266a461
Tim Staley <tim.staley@physics.ox.ac.uk>
2015-12-08 15:30:50 +0000

    Helper classes - restructure, document.

--- voeventdb/remote/helpers.py
+++ voeventdb/remote/helpers.py
@@ -13,46 +13,101 @@ import pprint
 
 class SkyEvent(object):
     """
-    Represents the most basic attributes of an observed event on sky.
+    Represents universal attributes of an observed event on sky.
 
-    Namely:
-     - position
-     - position error
-     - timestamp of event
+    I.e. the most basic details that we expect to find in all packets
+    reporting events with sky-positions.
+
+    Attributes:
+        position (:class:`astropy.coordinates.SkyCoord`): Best-estimate
+            sky-coordinates of the event being reported.
+
+        position error (:class:`astropy.coordinates.Angle`): Error-cone on the
+            position estimate.
+
+        timestamp of event (:class:`datetime.datetime`):
+            Timestamp for the reported event (UTC timezone).
+
     """
-    def __init__(self,
-                 sky_position,
-                 sky_position_error,
-                 event_timestamp):
-        self.position = sky_position
-        self.position_error = sky_position_error
-        self.timestamp = event_timestamp
+    def __init__(self, skyevent_dict):
+        d = skyevent_dict
+        self.position= SkyCoord(d['ra'], d['dec'], unit='deg')
+        self.position_error = Angle(d['error'], unit='deg')
+        self.timestamp = iso8601.parse_date(d['time'])
+
     def __repr__(self):
         return (str(self.position)
                 +' +/- ' + self.position_error.to_string(decimal=True)
                +' @ ' + self.timestamp.isoformat())
 
+    @staticmethod
+    def _parse_coords_dict(coords_dict):
+        d = coords_dict
+        posn = SkyCoord(d['ra'], d['dec'], unit='deg')
+        posn_error = Angle(d['error'], unit='deg')
+        time = iso8601.parse_date(d['time'])
+        return SkyEvent(sky_position=posn,
+                        sky_position_error=posn_error,
+                        event_timestamp=time)
+
 class Synopsis(object):
+    """
+    Parses the output from the 'synopsis' endpoint into a class-object.
+
+    Attributes:
+        author_datetime (:class:`datetime.datetime`): The ``Who.Date`` timestamp.
+            (Parsed to datetime, UTC timezone). May be ``None`` if no timestamp
+            present in the VOEvent packet.
+        author_ivorn (string): The ``Who.AuthorIVORN`` entry. May be
+            None if no entry present in the VOEvent packet.
+        ivorn (string)
+        received (:class:`datetime.datetime`): Dates exactly when the packet
+            was loaded into this instance of voeventdb.
+        role (string)
+        stream (string)
+        version (string)
+        sky_events (list): A list of :class:`SkyEvent` objects.
+            These are parsed from the `WhereWhen` section of a VOEvent
+            packet, if possible. (If the packet has no WhereWhen data,
+            this is an empty list).
+        coords (list): :class:`astropy.coordinates.SkyCoord` positions.
+            This is a syntax-shortcut, the positions are just those of the
+            :attr:`.sky_events` entries, if any are present.
+        references (list): References to other VOEvent packets.
+            This is a list of dictionaries representing any references
+            present in the ``Citations`` section of the VOEvent packet.
+
+    """
     def __init__(self, synopsis_dict, api_version_string='apiv0'):
-        self._synopsis_dict = synopsis_dict.copy()
 
-        self.sky_events = [self._parse_coords_dict(d)
-                           for d in synopsis_dict['coords']]
-
-        self.coords = [e.position for e in self.sky_events]
-
-        self.references = self._synopsis_dict['refs']
 
         voevent_dict = synopsis_dict['voevent']
-        self.author_datetime = iso8601.parse_date(
-            voevent_dict['author_datetime'])
-        self.author_ivorn = voevent_dict['author_ivorn']
+
+        self.author_datetime = None
+        if voevent_dict['author_datetime']:
+            self.author_datetime = iso8601.parse_date(
+                voevent_dict['author_datetime'])
+
+        self.author_ivorn = None
+        if voevent_dict['author_ivorn']:
+            self.author_ivorn = voevent_dict['author_ivorn']
+
         self.ivorn = voevent_dict['ivorn']
         self.received = iso8601.parse_date(
             voevent_dict['received'])
         self.role = voevent_dict['role']
         self.stream = voevent_dict['stream']
         self.version = voevent_dict['version']
+
+        self._synopsis_dict = synopsis_dict.copy()
+
+        self.sky_events = [SkyEvent(d) for d in synopsis_dict['coords']]
+
+        self.coords = [e.position for e in self.sky_events]
+
+        self.references = self._synopsis_dict['refs']
+
+
 
     def __repr__(self):
         # Can be pasted back in to recreate the object
@@ -71,12 +126,4 @@ class Synopsis(object):
         pp = pprint.PrettyPrinter(indent=2)
         return pp.pformat(display_vars)
 
-    @staticmethod
-    def _parse_coords_dict(coords_dict):
-        d = coords_dict
-        posn = SkyCoord(d['ra'], d['dec'], unit='deg')
-        posn_error = Angle(d['error'], unit='deg')
-        time = iso8601.parse_date(d['time'])
-        return SkyEvent(sky_position=posn,
-                        sky_position_error=posn_error,
-                        event_timestamp=time)
+
